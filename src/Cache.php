a86ada3012f1633f03ca27c00c270c1d37c0cdd4
Jakub Piasecki <jakub@unity3d.com>
2019-02-11 10:01:21 +0100

    Update to keep the code inline with symfony/flex

--- src/Cache.php
+++ src/Cache.php
@@ -3,6 +3,10 @@
 namespace zaporylie\ComposerDrupalOptimizations;
 
 use Composer\Cache as BaseCache;
+use Composer\IO\IOInterface;
+use Composer\Semver\Constraint\Constraint;
+use Composer\Semver\VersionParser;
+use Composer\Util\Filesystem;
 
 /**
  * Class Cache
@@ -12,12 +16,27 @@ class Cache extends BaseCache
 {
 
     /**
+     * @var array
+     */
+    protected $packages = [];
+
+    /**
+     * @var \Composer\Semver\VersionParser
+     */
+    protected $versionParser;
+
+    public function __construct(IOInterface $io, $cacheDir, $whitelist = 'a-z0-9.', Filesystem $filesystem = null) {
+        parent::__construct($io, $cacheDir, $whitelist, $filesystem);
+        $this->versionParser = new VersionParser();
+    }
+
+    /**
      * {@inheritdoc}
      */
     public function read($file)
     {
         $content = parent::read($file);
-        foreach (array_keys($this->getLowestTags()) as $key) {
+        foreach (array_keys($this->packages) as $key) {
             list($provider, ) = explode('/', $key, 2);
             if (0 === strpos($file, "provider-$provider\$")) {
                 $content = json_encode($this->removeLegacyTags(json_decode($content, true)));
@@ -35,29 +54,70 @@ class Cache extends BaseCache
      */
     public function removeLegacyTags(array $data)
     {
-        foreach ($this->getLowestTags() as $package => $lowestVersion) {
-            if (!isset($data['packages'][$package][$lowestVersion])) {
+        // Skip if the list of packages is empty.
+        if (!$this->packages || empty($data['packages'])) {
+            return $data;
+        }
+
+        // Skip if none of the packages was found.
+        if (!array_diff_key($data['packages'], $this->packages)) {
+            return $data;
+        }
+
+        foreach ($this->packages as $packageName => $packageVersionConstraint) {
+            if (!isset($data['packages'][$packageName])) {
                 continue;
             }
-            foreach ($data['packages'] as $package => $versions) {
-                foreach ($versions as $version => $composerJson) {
-                    if (version_compare($version, $lowestVersion, '<')) {
-                        unset($data['packages'][$package][$version]);
-                    }
+            $packages = [];
+            $specificPackage = $data['packages'][$packageName];
+            foreach ($specificPackage as $version => $composerJson) {
+                if ('dev-master' === $version) {
+                    $normalizedVersion = $this->versionParser->normalize($composerJson['extra']['branch-alias']['dev-master']);
+                } else {
+                    $normalizedVersion = $composerJson['version_normalized'];
+                }
+                if (!$this->versionParser->parseConstraints($packageVersionConstraint)->matches(new Constraint('==',
+                  $normalizedVersion))) {
+                    $packages += isset($composerJson['replace']) ? $composerJson['replace'] : [];
+                } else {
+                    unset($specificPackage[$version]);
                 }
             }
-            break;
+
+            // Ignore requirements: their intersection with versions of the package gives empty result.
+            if (!$specificPackage) {
+                continue;
+            }
+            $data['packages'][$packageName] = $specificPackage;
+
+            unset($specificPackage['dev-master']);
+            foreach ($data['packages'] as $name => $versions) {
+                if (!isset($packages[$name]) || null === $devMasterAlias = (isset($versions['dev-master']['extra']['branch-alias']['dev-master']) ? $versions['dev-master']['extra']['branch-alias']['dev-master'] : null)) {
+                    continue;
+                }
+                $devMaster = $versions['dev-master'];
+                $versions = array_intersect_key($versions, $specificPackage);
+                if (!$this->versionParser->parseConstraints($packageVersionConstraint)->matches(new Constraint('==',
+                  $this->versionParser->normalize($devMasterAlias)))) {
+                    $versions['dev-master'] = $devMaster;
+                }
+                if ($versions) {
+                    $data['packages'][$name] = $versions;
+                }
+            }
         }
         return $data;
+
     }
 
     /**
-     * Gets list of lowest allowed tags.
+     * @param array $packages
      *
-     * @return array
+     * @return $this
      */
-    private function getLowestTags()
-    {
-        return Config::getInstance()->getLowestTags();
+    public function setLegacyVersionConstraints(array $packages) {
+      $this->packages = $packages;
+      return $this;
     }
+
 }
--- src/Plugin.php
+++ src/Plugin.php
@@ -8,30 +8,47 @@ use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
 use Composer\Repository\RepositoryFactory;
 use Composer\Repository\RepositoryManager;
+use Composer\Semver\Semver;
 
 class Plugin implements PluginInterface
 {
+
     public function activate(Composer $composer, IOInterface $io)
     {
-        // Set lowest-tags from composer.json, 'extra' section.
-        if (($extra = $composer->getPackage()->getExtra()) && isset($extra['lowest-tags'])) {
-            if (is_array($extra['lowest-tags'])) {
-                Config::getInstance()->setLowestTags($extra['lowest-tags']);
+        // Set default version constraints based on the composer requirements.
+        $extra = $composer->getPackage()->getExtra();
+        $packages = $composer->getPackage()->getRequires();
+        if (empty($extra['composer-drupal-optimizations']['legacy']) && isset($packages['drupal/core'])) {
+            for ($i = 0; $i < 10; $i++) {
+                if (Semver::satisfies(sprintf("8.%d", $i), $packages['drupal/core']->getConstraint()->getPrettyString())) {
+                    break;
+                }
             }
-            elseif ($io->isVerbose()) {
-                $io->writeError('Extra value "lowest-tags" was provided in wrong format - array expected.');
+            if ($i >= 5) {
+                $extra['composer-drupal-optimizations']['legacy'] = [
+                    'symfony/symfony' => '<3.4',
+                ];
+                if ($io->isVeryVerbose() || TRUE) {
+                    $io->write('Legacy tags were not explicitly set so the zaporylie/composer-drupal-optimizations did the calculation:');
+                    foreach ($extra['composer-drupal-optimizations']['legacy'] as $package => $version) {
+                        $io->write(sprintf('extra.commerce-drupal-optimizations.legacy.%s: \'%s\'', $package, $version));
+                    }
+                }
             }
         }
 
         $rfs = Factory::createRemoteFilesystem($io, $composer->getConfig());
         $manager = RepositoryFactory::manager($io, $composer->getConfig(), $composer->getEventDispatcher(), $rfs);
-        $setRepositories = \Closure::bind(function (RepositoryManager $manager) {
+        $setRepositories = \Closure::bind(function (RepositoryManager $manager) use ($extra) {
             $manager->repositoryClasses = $this->repositoryClasses;
             $manager->setRepositoryClass('composer', TruncatedComposerRepository::class);
             $manager->repositories = $this->repositories;
             $i = 0;
             foreach (RepositoryFactory::defaultRepos(null, $this->config, $manager) as $repo) {
                 $manager->repositories[$i++] = $repo;
+                if ($repo instanceof TruncatedComposerRepository && isset($extra['composer-drupal-optimizations']['legacy'])) {
+                  $repo->setLegacyVersionConstraints($extra['composer-drupal-optimizations']['legacy']);
+                }
             }
             $manager->setLocalRepository($this->getLocalRepository());
         }, $composer->getRepositoryManager(), RepositoryManager::class);
